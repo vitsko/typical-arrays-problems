b8cb68fdc123f7d5ea705632f6ffcf5f456e7b49
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-03-21 20:00:29 +0400

    feat: add solution task 'Typical Arrays Problems'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
 
-exports.min = function min (array) {
-  return 0;
+  return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+
+  return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+
+  let sum = array.reduce((sum, item) => sum + item, 0);
+
+  return sum / array.length;
 }
